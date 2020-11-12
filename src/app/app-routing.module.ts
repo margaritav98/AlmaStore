7cdf350863af04552774c8e4f9bc742395f3a9ac
Margarita <mivillafanep@unincca.edu.co>
2020-11-11 19:35:59 -0500

    Avance frontend listo para conexión con api

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+// components
+import { ProductListComponent } from "./components/mujeres/product-list/product-list.component";
+import { BlusasListComponent } from "./components/mujeres/details/blusas-list/blusas-list.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "mujeres", component: ProductListComponent },
+  { path: "mujeres/blusas", component: BlusasListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { PreHeaderComponent } from './components/header/pre-header/pre-header.component';
-import { HeaderComponent } from './components/header/header/header.component';
-import { AngularFontAwesomeModule } from 'angular-font-awesome';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { PreHeaderComponent } from "./components/header/pre-header/pre-header.component";
+import { HeaderComponent } from "./components/header/header/header.component";
+import { AngularFontAwesomeModule } from "angular-font-awesome";
+import { CarouselComponent } from "./components/carousel/carousel.component";
+import { ProductListComponent } from './components/mujeres/product-list/product-list.component';
+import { BlusasListComponent } from './components/mujeres/details/blusas-list/blusas-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PreHeaderComponent,
-    HeaderComponent
+    HeaderComponent,
+    CarouselComponent,
+    ProductListComponent,
+    BlusasListComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    AngularFontAwesomeModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, AngularFontAwesomeModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
